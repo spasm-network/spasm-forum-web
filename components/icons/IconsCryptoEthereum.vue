0cb34ac56358273bfb33a1080758bb5914543096
degenrocket <noreply@degenrocket.space>
2026-01-31 23:09:43 +0000

    feat: add crypto icons

---
+++ components/icons/IconsCryptoEthereum.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- Icon: Ethereum -->
+  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="28" height="28"
+     viewBox="0 0 327.5 533.3" style="enable-background:new 0 0 327.5 533.3;" xml:space="preserve">
+  <path fill="currentColor" class="st0" d="M163.7,197.2V0L0,271.6L163.7,197.2z"/>
+  <path fill="currentColor" class="st1" d="M163.7,368.4V197.2L0,271.6L163.7,368.4z M163.7,197.2l163.7,74.4L163.7,0V197.2z"/>
+  <path fill="currentColor" class="st2" d="M163.7,197.2v171.2l163.7-96.8L163.7,197.2z"/>
+  <path fill="currentColor" class="st0" d="M163.7,399.4L0,302.7l163.7,230.7V399.4z"/>
+  <path fill="currentColor" class="st1" d="M327.5,302.7l-163.8,96.7v134L327.5,302.7z"/>
+  </svg>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped></style>
